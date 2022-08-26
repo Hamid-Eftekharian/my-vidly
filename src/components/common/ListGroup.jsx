bfeb6ca14ed90114684d452bbbf39f0b8f57f978
Hamid Eftekharian <hamid.eftekharian@yahoo.com>
2022-08-26 21:52:01 +1000

    Added ListGroup

--- src/components/common/ListGroup.jsx
+++ src/components/common/ListGroup.jsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 
 const ListGroup = (props) => {
-  const { items, textProperty, valueProperty, onItemSelect } = props;
+  const { items, selectedItem, textProperty, valueProperty, onItemSelect } =
+    props;
 
   return (
     <ul className="list-group">
       {items.map((item) => (
         <li
           key={item[valueProperty]}
-          className="list-group-item"
-          onClick={() => onItemSelect({ item })}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+          onClick={() => onItemSelect(item)}
         >
           {item[textProperty]}
         </li>

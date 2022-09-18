22d910753a304d497f9ff26ee1436781a0759c8b
Hamid Eftekharian <hamid.eftekharian@yahoo.com>
2022-09-18 18:41:04 +1000

    Updated Rentals Component

--- src/components/Rentals.jsx
+++ src/components/Rentals.jsx
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import _ from "lodash";
 
 class Rentals extends Component {
-  state = {};
+  state = {
+    cats: [
+      { owner: "male", name: "Alicia" },
+      { owner: "female", name: "Jessi" },
+      { owner: "male", name: "Maloos" },
+      { owner: "female", name: "Catty" },
+    ],
+  };
+
+  getSorted = () => {
+    const cats = _.orderBy(this.state.cats, ["owner", "name"]);
+    // const males = cats.filter((c) => c.owner==='male');
+    // const females = cats.filter((c) => c.owner !== 'female')
+    this.setState({ cats });
+    console.log(cats);
+  };
+
   render() {
-    return <h1>Rental Pages</h1>;
+    return (
+      <div>
+        <h1>Rental Pages</h1>
+        <ul>
+          {this.state.cats.map((item) => (
+            <li>
+              Cat Owner: {item.owner}, Cat Name: {item.name}
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => this.getSorted()}>Get Sorted</button>
+      </div>
+    );
   }
 }
 
